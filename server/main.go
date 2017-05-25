7318f3235a0d30f6991178c8eaf596cce446aa24
Mihnea Spirescu <mihnea.spirescu@lendr.io>
2017-05-25 17:07:24 +0100

    not working yet  but compiles

--- server/main.go
+++ server/main.go
@@ -9,16 +9,49 @@ import (
 	"strings"
 
 	"encoding/json"
+	"os"
 )
-
 
 type db map[string]map[string]bool
 
+type Sibling struct {
+	address string
+	conn    net.Conn
+}
+type Update struct {
+	topic string
+	value string
+}
+
+var siblings []Sibling
+
+func set(topic string, address string) string {
+	return "SET " + topic + " " + address
+}
+
+func writeToSibligs(c chan Update) {
+
+	for val := range c {
+		fmt.Printf("==> Received a message to send to siblings %v\n", val)
+		for _, s := range siblings {
+			fmt.Printf("=> Sending to %v\n", s.address)
+			go fmt.Fprint(s.conn, set(val.topic, val.value))
+			fmt.Println("i don't get it")
+		}
+	}
+
+	fmt.Println("closing .....")
+
+}
+
 func handleConnection(conn net.Conn, data db) {
 
-
-
 	scanner := bufio.NewScanner(conn)
+
+	toSiblings := make(chan Update)
+
+	go writeToSibligs(toSiblings)
+
 	for scanner.Scan() {
 
 		a := scanner.Text()
@@ -31,18 +64,26 @@ func handleConnection(conn net.Conn, data db) {
 
 		switch w[0] {
 		case "SET":
-			fmt.Printf("will set -> %v with value: %v\n", w[1], w[2] )
+			fmt.Printf("will set -> %v with value: %v\n", w[1], w[2])
 			if len(w) != 3 {
 				fmt.Fprint(conn, "==> SET must have a value\n")
 				continue
 			}
 			if _, ok := data[w[1]]; !ok {
 				data[w[1]] = map[string]bool{w[2]: true, }
+				toSiblings <- Update{
+					topic: w[1],
+					value: w[2],
+				}
 				continue
 			}
 
 			mapS := data[w[1]]
 			mapS[w[2]] = true
+			toSiblings <- Update{
+				topic: w[1],
+				value: w[2],
+			}
 
 		case "GET":
 			if len(w) != 2 {
@@ -75,8 +116,6 @@ func handleConnection(conn net.Conn, data db) {
 		case "DEL":
 			fmt.Fprintf(conn, "==> DELETED: %v\n", data[w[1]])
 			delete(data, w[1])
-		case "EXIT":
-			conn.Close()
 		default:
 			fmt.Fprintf(conn, "==> %v\n", "Command not found")
 		}
@@ -88,18 +127,46 @@ func handleConnection(conn net.Conn, data db) {
 
 func main() {
 
-	ln, err := net.Listen("tcp", ":3500")
+	// args[0] => portServer
+	// args[1] => all other instances addresses
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		servers := strings.Split(args[1], ",")
+
+		fmt.Println(servers)
+
+		for _, s := range servers {
+
+			conn, err := net.Dial("tcp", s)
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			siblings = append(siblings, Sibling{
+				address: s,
+				conn:    conn,
+			})
+		}
+
+		fmt.Println(siblings)
+	}
+
+	ln, err := net.Listen("tcp", ":"+args[0])
 	if err != nil {
+		log.Fatal(err)
 		// handle error
 	}
-	data := make(db)
 
+	data := make(db)
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			// handle error
 			log.Panic(err)
+
 		}
 		go handleConnection(conn, data)
 	}
